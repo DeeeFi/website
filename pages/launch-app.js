96eb1e32510ce79a73d1b461922acc5f1d4d32a7
xywei <wxy0516@gmail.com>
2023-08-30 08:54:12 -0500

    Prelim radar chart

--- pages/launch-app.js
+++ pages/launch-app.js
@@ -42,6 +42,102 @@ import {
 import '@rainbow-me/rainbowkit/styles.css';
 import { ConnectButton } from '@rainbow-me/rainbowkit';
 
+import {
+  Chart as ChartJS,
+  RadialLinearScale,
+  PointElement,
+  LineElement,
+  Filler,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Radar } from 'react-chartjs-2';
+
+ChartJS.register(
+  RadialLinearScale,
+  PointElement,
+  LineElement,
+  Filler,
+  Tooltip,
+  Legend
+);
+
+var randomScalingFactor = function() {
+  return Math.round(Math.random() * 100);
+};
+
+var chartColors = {
+	red: 'rgb(255, 99, 132)',
+	orange: 'rgb(255, 159, 64)',
+	yellow: 'rgb(255, 205, 86)',
+	green: 'rgb(75, 192, 192)',
+	blue: 'rgb(54, 162, 235)',
+	purple: 'rgb(153, 102, 255)',
+	grey: 'rgb(231,233,237)'
+};
+
+import Chart from 'chart.js/auto'
+var color = Chart.helpers.color;
+
+const radar_options = {};
+
+
+const radar_data = {
+    labels: [
+      ["Eating", "Dinner"],
+      ["Drinking", "Water"], "Sleeping", ["Designing", "Graphics"], "Coding", "Cycling", "Running"
+    ],
+    datasets: [{
+      label: "My First dataset",
+      backgroundColor: color(chartColors.red).alpha(0.2).rgbString(),
+      borderColor: chartColors.red,
+      pointBackgroundColor: chartColors.red,
+      data: [
+        randomScalingFactor(),
+        randomScalingFactor(),
+        randomScalingFactor(),
+        randomScalingFactor(),
+        randomScalingFactor(),
+        randomScalingFactor(),
+        randomScalingFactor()
+      ]
+    }, {
+      label: "My Second dataset",
+      borderColor: chartColors.blue,
+      pointBackgroundColor: chartColors.blue,
+      data: [
+        randomScalingFactor(),
+        randomScalingFactor(),
+        randomScalingFactor(),
+        randomScalingFactor(),
+        randomScalingFactor(),
+        randomScalingFactor(),
+        randomScalingFactor()
+      ]
+    }, ],
+    options: {
+        legend: {
+            position: 'top',
+            labels: { fontColor: 'white' }
+        },
+        title: {
+            display: true,
+            text: 'Chart.js Radar Chart',
+            fontColor: 'white'
+        },
+        scale: {
+            ticks: {
+                beginAtZero: true,
+                fontColor: 'white', // labels such as 10, 20, etc
+                showLabelBackdrop: false // hide square behind text
+            },
+            pointLabels: { fontColor: 'white' /* labels around the edge like 'Running' */ },
+            gridLines: { color: 'rgba(255, 255, 255, 0.2)' },
+            angleLines: { color: 'white' /* lines radiating from the center */ }
+        }
+    }
+};
+
 const { chains, publicClient, webSocketPublicClient } = configureChains(
   [
     mainnet,
@@ -168,6 +264,7 @@ function WalletInfo() {
 
         <Section>
         Radar chart
+        <Radar data={radar_data} options={radar_options} />
         </Section>
 
         <Section>
